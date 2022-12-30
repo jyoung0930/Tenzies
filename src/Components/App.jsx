9f2d41601efb25138d4e0e401761c902bdeb44f6
jyoung0930 <jaronyoung1@gmail.com>
2022-12-30 11:58:46 -0600

    Continued Development
    Started adding CSS and Dice logic.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,3 +1,29 @@
+import Die from "./Die";
+
 export default function App() {
-  return <main></main>;
+  function allNewDice() {
+    const randomNumbers = [];
+    for (let i = 0; i < 10; i++) {
+      randomNumbers.push(Math.ceil(Math.random() * 6) + 1);
+    }
+    return randomNumbers;
+  }
+  console.log(allNewDice());
+
+  return (
+    <main>
+      <div className="die-grid">
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+        <Die number={1} />
+      </div>
+    </main>
+  );
 }
---
+++ src/Components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props) {
+  return (
+    <div className="die-box">
+      <h1 className="die-number">{props.number}</h1>
+    </div>
+  );
+}
