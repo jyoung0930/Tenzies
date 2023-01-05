267eeefa9dbc574ed5c3d2d0d7c658a0c8aeed9e
jyoung0930 <jaronyoung1@gmail.com>
2023-01-05 14:41:55 -0600

    continued development
    Additional dice logic

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,37 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Die from "./Die";
 
 export default function App() {
   const [randomNumbers, updatedRandomNumbers] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = randomNumbers.every((die) => die.isHeld);
+    const firstValue = randomNumbers[0].value;
+    const allSameValue = randomNumbers.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("You won!");
+    }
+  }, [randomNumbers]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
   function allNewDice() {
     const randomValues = [];
     for (let i = 0; i < 10; i++) {
-      randomValues.push({
-        value: Math.ceil(Math.random() * 6) + 1,
-        isHeld: false,
-        id: nanoid(),
-      });
+      randomValues.push(generateNewDie());
     }
     return randomValues;
   }
 
   function holdDice(id) {
-    updatedRandomNumbers((prev) => {
+    updatedRandomNumbers((prev) =>
       prev.map((item) => {
-        return item.id === id ? { ...item, isHeld: !prev.isHeld } : item;
-      });
-    });
+        return item.id === id ? { ...item, isHeld: !item.isHeld } : item;
+      })
+    );
   }
-
-  // function holdDice(id) {
-  //   updatedRandomNumbers((oldDice) =>
-  //     oldDice.map((die) => {
-  //       return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-  //     })
-  //   );
-  // }
 
   const values = randomNumbers.map((num) => {
     return (
@@ -47,11 +54,20 @@ export default function App() {
   });
 
   function handleRoll() {
-    updatedRandomNumbers(allNewDice());
+    updatedRandomNumbers((prev) =>
+      prev.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="die-grid">{values}</div>
       <button onClick={handleRoll} className="roll-btn">
         Roll
